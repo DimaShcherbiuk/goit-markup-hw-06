3bec7d7872fac5372b927755197ea63ba42e8212
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-06-08 18:57:55 +0300

    fix href

--- js/modal.js
+++ js/modal.js
@@ -1,29 +1,36 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-open");
+    refs.modal.classList.toggle('is-open');
   }
 })();
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[menu-modal-open]"),
-    closeModalBtn: document.querySelector("[menu-modal-close]"),
-    modal: document.querySelector("[menu-modal]"),
+    openModalBtn: document.querySelector('[menu-modal-open]'),
+    closeModalBtn: document.querySelector('[menu-modal-close]'),
+    modal: document.querySelector('[menu-modal]'),
+    modalLinks: document.querySelectorAll('[menu-modal] a[href^="#"]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-open");
+    refs.modal.classList.toggle('is-open');
   }
+
+  refs.modalLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.modal.classList.remove('is-open');
+    });
+  });
 })();
